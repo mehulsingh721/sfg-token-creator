1a0e67823b9f8f30aaa5ea0417ba5a6a752b2d41
Mehul Singh <mehulsingh072001@gmail.com>
2024-07-09 16:39:34 +0530

    fd

--- app/constants/app.ts
+++ app/constants/app.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const ADMIN_WALLET = new PublicKey(
-  "Ea4q8Cmd3bTv9AML8mh8BWc5d3tbnqqFMZ5HsGKG8Q7u"
+  "J5k6w9EYgUc7Vxnx5FN7b7qqeWc7Vb5oT53wnnkd8pxY"
 );
 
 export const NETWORK_URL =
